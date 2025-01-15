776525e11434fdd9a2c80033a7a680d08e1471f2
kafka6666 <140368515+kafka6666@users.noreply.github.com>
2025-01-15 14:10:02 +0600

    start server from main.go file

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/kafka6666/rest-api-go/internal/config"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Welcome to Students API")
+
+	// load config file
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Welcome to Students API"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	// setup server
+	server := &http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started on %s", cfg.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
 
 }
